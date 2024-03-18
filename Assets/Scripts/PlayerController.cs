2ee75f899058a932f0c271019547b30e9346636a
Kacper Dziedzic <kacper.dziedzic@teb.edu.pl>
2024-03-18 10:34:50 +0100

    fddd

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -32,7 +32,13 @@ public class PlayerController : MonoBehaviour
         //pomnó¿ przez prêdkoœæ lotu
         movement *= flySpeed;
         //dodaj ruch do obiektu
-        transform.position += movement;
+        //zmiana na fizyke
+        // --- transform.position += movement;
+
+        //komponent fizyki wewn¹trz gracza
+        Rigidbody rb = GetComponent<Rigidbody>();
+        //dodaj si³e - do przodu statku w trybie zmiany prêdkoœci
+        rb.AddForce(movement, ForceMode.VelocityChange);
 
 
         //obrót
@@ -48,5 +54,18 @@ public class PlayerController : MonoBehaviour
         //nie mo¿emy u¿yæ += poniewa¿ unity u¿ywa Quaternionów do zapisu rotacji
         transform.Rotate(rotation);
 
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //uruchamia siê automatycznie jeœli zetkniemy sie z innym coliderem
+
+        //sprawdz czy dotknêliœmy asteroidy
+        if (collision.collider.transform.CompareTag("Asteroid"))
+        {
+            Debug.Log("Boom!");
+            //pauza
+            Time.timeScale = 0;
+        }
     }
 }
